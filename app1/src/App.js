b5c9ecf8e801a69eb81bb2e57d068ad2dd4de0d3
SKarmakar98 <subhankar_@outlook.com>
2024-03-03 08:17:15 +0530

    .

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
 <Navbar title="App1"/>
+<Footer/>
     </div>
   );
 }
