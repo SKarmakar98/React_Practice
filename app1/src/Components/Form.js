5bf53ca87ced4ec5b01bd6b02f5c17aad2a42f4b
Subhankar Karmakar <125754757+SKarmakar98@users.noreply.github.com>
2024-03-03 20:51:15 +0530

    Update Form.js

--- app1/src/Components/Form.js
+++ app1/src/Components/Form.js
@@ -57,29 +57,7 @@ export default function Form() {
             placeholder={text}
             onChange={changetext}
           />
-          {/* <small id="emailHelp" className="form-text text-muted">
-          We'll never share your email with anyone else.
-        </small> */}
         </div>
-        {/* <div className="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-          placeholder="Password"
-        />
-      </div> */}
-        {/* <div className="form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" for="exampleCheck1">
-          Check me out
-        </label>
-      </div> */}
         <button
           type="submit"
           className="btn btn-primary mt-2 m-2"
